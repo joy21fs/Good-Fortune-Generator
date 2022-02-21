bf338adc1c67e60a3c8dfe4adb1e7fe136ea92e0
joy21fs <95619834+joy21fs@users.noreply.github.com>
2022-02-21 17:55:23 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+const num = Math.floor(Math.random() * 6) + 1;
+
+function refresh() {
+  setTimeout(function () {
+    document.querySelector(".shuffle-btn").click();
+  }, 10);
+}
+
+document.addEventListener("load", refresh);
+
+document.querySelector(".shuffle-btn").addEventListener("click", function () {
+  document.querySelector("main").classList.add("shuffle");
+
+  setTimeout(function () {
+    document.querySelector("main").classList.remove("shuffle");
+  }, 1000);
+});
+
+const scrolls = document.querySelectorAll(".scroll");
+
+scrolls.forEach((scroll) => {
+  const notPicked = () => {
+    scroll.setAttribute("src", "images/scrolls/scroll_" + scroll.id + ".png");
+  };
+
+  scroll.addEventListener("mouseover", function () {
+    this.setAttribute(
+      "src",
+      "images/scrolls/picked/scroll" + this.id + "_picked.png"
+    );
+  });
+
+  scroll.addEventListener("mouseout", notPicked);
+
+  scroll.addEventListener("click", function () {
+    this.removeEventListener("mouseout", notPicked);
+
+    document.querySelector(".show-poem").classList.add("show");
+  });
+});
+
+document.querySelector(".show-poem").addEventListener("click", function () {
+  document.querySelector(".poem").classList.add("show");
+  document
+    .querySelector(".poem")
+    .setAttribute("src", "images/poems/fortune-poem-" + num + ".jpg");
+  setTimeout(function () {
+    document.querySelector(".reset").classList.add("show");
+  }, 1500);
+});
+
+document.querySelector(".reset").addEventListener("click", function () {
+  window.location.reload();
+});
